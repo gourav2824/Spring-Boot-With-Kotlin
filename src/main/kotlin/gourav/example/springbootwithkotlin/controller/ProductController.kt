2466c342f8c869819d09d66ec7379d9293099ab7
Gourav <gourav2824@gmail.com>
2023-06-08 00:40:46 +0530

    Add: endpoint to get a product with id

--- src/main/kotlin/gourav/example/springbootwithkotlin/controller/ProductController.kt
+++ src/main/kotlin/gourav/example/springbootwithkotlin/controller/ProductController.kt
@@ -3,6 +3,7 @@ package gourav.example.springbootwithkotlin.controller
 import gourav.example.springbootwithkotlin.model.Product
 import gourav.example.springbootwithkotlin.service.ProductService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,6 +16,11 @@ class ProductController(val productService: ProductService) {
     @PostMapping("/add")
     fun addProduct(@RequestBody product: Product): Product {
         return productService.addProduct(product)
+    }
+
+    @GetMapping("/{id}")
+    fun getProduct(@PathVariable id: Int): Product? {
+        return productService.getProduct(id)
     }
 
     @GetMapping
--- src/main/kotlin/gourav/example/springbootwithkotlin/service/ProductService.kt
+++ src/main/kotlin/gourav/example/springbootwithkotlin/service/ProductService.kt
@@ -3,11 +3,16 @@ package gourav.example.springbootwithkotlin.service
 import gourav.example.springbootwithkotlin.model.Product
 import gourav.example.springbootwithkotlin.repository.ProductRepository
 import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrElse
 
 @Service
 class ProductService(val productRepository: ProductRepository) {
     fun addProduct(product: Product): Product {
         return productRepository.save(product)
+    }
+
+    fun getProduct(id: Int): Product? {
+        return productRepository.findById(id).getOrElse { null }
     }
 
     fun getAllProducts(): MutableList<Product> {
